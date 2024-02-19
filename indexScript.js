8512e3cb4135a0b833239360165616358bbf2774
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-02-19 15:22:52 +0100

    Add random colors and darken the square on repeat
    Randomize the RGB code first time a square is hovered over.
    After the first time darken the color.

--- indexScript.js
+++ indexScript.js
@@ -9,14 +9,26 @@ function etchasketch(number){
         for(let y = 0; y<number; y++ ){
             let column = document.createElement("div");
             column.setAttribute('class', 'square column');
+            column.style.height = (960 / number)+"px";
+            column.style.width = (960 / number)+"px";
             row.appendChild(column);
         }
         gridContainer.appendChild(row);
 
     }
     gridContainer.addEventListener("mouseover", event=>{
-        if(event.target.classList.contains("square")){
-            event.target.style.backgroundColor = "blue";
+        if(event.target.classList.contains("square") && event.target.style.backgroundColor == ""){
+
+            event.target.style.backgroundColor = getRgbCode();
+            
+        }else if(event.target.classList.contains("square") && event.target.style.backgroundColor != ""){
+            let currentRGB = event.target.style.backgroundColor;
+            let splitRGB = currentRGB.match(/\d+/g);
+            
+            event.target.style.backgroundColor = `rgb(${splitRGB[0]*0.9},${splitRGB[1]*0.9},${splitRGB[2]*0.9})`
+
+
+
         }
 
         
@@ -27,8 +39,26 @@ function etchasketch(number){
     document.body.appendChild(gridContainer);
 }
 
-let getNumber = prompt("How big do you want your grid ?", 16);
+const randomBetween = (min, max)=> min + Math.floor(Math.random() * (max - min + 1));
+function getRgbCode(){
+    let r = randomBetween(0,255);
+    let g = randomBetween(0,255);
+    let b = randomBetween(0,255);
+   
+    return `rgb(${r},${g},${b})`;
+}
 
 
-etchasketch(getNumber);
+
+//let getNumber = prompt("How big do you want your grid ?", 16);
+
+let generateButton = document.querySelector("#generateButton");
+generateButton.addEventListener("click",()=>{
+    let gridContainer=document.querySelector(".gridContainer");
+    if(gridContainer != null){
+        gridContainer.remove();
+    }
+    etchasketch(prompt("How big do you want your grid ?", 16));
+});
+
 
